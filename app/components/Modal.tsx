690a0c67be31192082ea53ec82b27cab36736694
nowaveosu <nowaveosu@gmail.com>
2025-07-01 15:54:15 +0900

    [Fix] 모달 파일위치 이동

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -3,10 +3,9 @@ import React from 'react';
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
-  children: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose}) => {
   if (!isOpen) return null;
 
   return (
@@ -20,7 +19,25 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
             x
           </button>
         </div>
-        <div className="mt-2">{children}</div>
+        <div className="mt-2">
+          <h2 className="text-xl font-bold mb-4">꼬들7 소개</h2>
+          <p className="mb-2">꼬들7은 6번안에 맞춰야하는 한글 낱말퍼즐입니다!</p>
+          <p className="mb-2">예를들어 {' '}
+            <span className="bg-gray-200 px-1 rounded">ㅌㅗㅇㄴㅏㅁㅜ</span>를 입력시</p>
+          <p className="mb-2">
+            정답과 위치와 구성요소가 같다면 배경이{' '}<br />
+            <span className="bg-green-400 px-1 rounded">초록색</span>으로 변하고,
+          </p>
+          <p className="mb-2">
+            위치는 다르고 구성요소가 맞다면 배경이{' '}<br />
+            <span className="bg-yellow-400 px-1 rounded">노란색</span>으로 변하고,
+          </p>
+          <p className="mb-2">
+            위치도 다르고 포함도 안됐다면 배경이{' '}<br />
+            <span className="bg-gray-400 px-1 rounded">회색</span>으로 변합니다.
+          </p>
+          <p>자음 모음으로 7칸을 꽉채워서 정답을 맞춰보세요!</p>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -209,25 +209,8 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <h2 className="text-xl font-bold mb-4">꼬들7 소개</h2>
-        <p className="mb-2">꼬들7은 6번안에 맞춰야하는 한글 낱말퍼즐입니다!</p>
-        <p className="mb-2">예를들어 {' '}
-          <span className="bg-gray-200 px-1 rounded">ㅌㅗㅇㄴㅏㅁㅜ</span>를 입력시</p>
-        <p className="mb-2">
-          정답과 위치와 구성요소가 같다면 배경이{' '}<br />
-          <span className="bg-green-400 px-1 rounded">초록색</span>으로 변하고,
-        </p>
-        <p className="mb-2">
-          위치는 다르고 구성요소가 맞다면 배경이{' '}<br />
-          <span className="bg-yellow-400 px-1 rounded">노란색</span>으로 변하고,
-        </p>
-        <p className="mb-2">
-          위치도 다르고 포함도 안됐다면 배경이{' '}<br />
-          <span className="bg-gray-400 px-1 rounded">회색</span>으로 변합니다.
-        </p>
-        <p>자음 모음으로 7칸을 꽉채워서 정답을 맞춰보세요!</p>
-      </Modal>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+  
       <div className='flex justify-between w-108 max-lg:w-68 mb-2 text-lg'>
         <header className="text-xl p-1.5">꼬들7</header>
         <div className='bg-gray-200 rounded-lg text-xl p-1'>힌트: 헬스</div>
